5edeb92696753fb1485e70656fbac1ff0a62428d
edward <edwardlfish@gmail.com>
2025-07-09 23:29:46 +0300

    First commit

---
+++ src/main/java/telran/java58/post/dto/NewCommentDto.java
@@ -0,0 +1,8 @@
+package telran.java58.post.dto;
+
+import lombok.Getter;
+
+@Getter
+public class NewCommentDto {
+    private String message;
+}
---
+++ src/main/java/telran/java58/post/dto/NewPostDto.java
@@ -0,0 +1,12 @@
+package telran.java58.post.dto;
+
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+public class NewPostDto {
+    private String title;
+    private String content;
+    private Set<String> tags;
+}
