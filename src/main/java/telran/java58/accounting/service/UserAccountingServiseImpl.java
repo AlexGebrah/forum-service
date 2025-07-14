a03a93cf534f3d88c84db0670c82842d7ec4dbcd
HPPro <alexisgebrah@gmail.com>
2025-07-14 20:45:40 +0300

    accounting

--- src/main/java/telran/java58/accounting/service/UserAccountingServiseImpl.java
+++ src/main/java/telran/java58/accounting/service/UserAccountingServiseImpl.java
@@ -1,6 +1,7 @@
 package telran.java58.accounting.service;
 
 import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import telran.java58.accounting.dao.UserAccountingRepository;
@@ -14,54 +15,75 @@ import telran.java58.accounting.model.UserAccount;
 @Service
 @RequiredArgsConstructor
 public class UserAccountingServiseImpl implements AccountingService{
-    private  final UserAccountingRepository accountingRepository;
+    private  final UserAccountingRepository userAccountingRepository;
     private final ModelMapper modelMapper;
 
     @Override
     public UserDto register(UserCreationDto userCreationDto) {
-        if (accountingRepository.existsById(userCreationDto.getLogin())) {
+        if (userAccountingRepository.existsById(userCreationDto.getLogin())) {
             throw new UserAlreadyExistsException(userCreationDto.getLogin());
         }
 
         UserAccount userAccount = modelMapper.map(userCreationDto, UserAccount.class);
         userAccount.addRole("USER");
-        accountingRepository.save(userAccount);
+        String password = BCrypt.hashpw(userCreationDto.getPassword(), BCrypt.gensalt());
+        userAccount.setPassword(password);
+        userAccountingRepository.save(userAccount);
         return modelMapper.map(userAccount, UserDto.class);
     }
 
     @Override
     public UserDto login(String login) {
-        UserAccount userAccount = accountingRepository.findById(login).orElseThrow();
+        UserAccount userAccount = userAccountingRepository.findById(login).orElseThrow();
         return modelMapper.map(userAccount, UserDto.class);
     }
 
     @Override
     public UserDto deleteUser(String login) {
-        return null;
+        UserAccount userAccount = userAccountingRepository.findById(login).orElseThrow();
+        userAccountingRepository.delete(userAccount);
+        return modelMapper.map(userAccount, UserDto.class);
     }
 
     @Override
     public UserDto updateUser(String login, UserUpdateDto userUpdateDto) {
-        return null;
+        UserAccount userAccount = userAccountingRepository.findById(login).orElseThrow();
+        if(userUpdateDto.getFirstName() != null) {
+            userAccount.setFirstName(userUpdateDto.getFirstName());
+        }
+        if (userUpdateDto.getLastName() != null) {
+            userAccount.setLastName(userUpdateDto.getLastName());
+        }
+        return modelMapper.map(userAccountingRepository.save(userAccount), UserDto.class);
     }
 
     @Override
     public NewRolesDto addRole(String login, String role) {
-        return null;
+        UserAccount userAccount = userAccountingRepository.findById(login).orElseThrow();
+        userAccount.addRole(role);
+        userAccountingRepository.save(userAccount);
+        return modelMapper.map(userAccount, NewRolesDto.class);
     }
 
     @Override
     public NewRolesDto deleteRole(String login, String role) {
-        return null;
+        UserAccount userAccount = userAccountingRepository.findById(login).orElseThrow();
+        userAccount.removeRole(role);
+        userAccountingRepository.save(userAccount);
+        return modelMapper.map(userAccount, NewRolesDto.class);
     }
 
     @Override
-    public void changePassword(String password, String newPassword) {
-
+    public void changePassword(String login, String newPassword) {
+        UserAccount userAccount = userAccountingRepository.findById(login).orElseThrow();
+        String hashedPassword = BCrypt.hashpw(newPassword, BCrypt.gensalt());
+        userAccount.setPassword(hashedPassword);
+        userAccountingRepository.save(userAccount);
     }
 
     @Override
     public UserDto getUser(String login) {
-        return null;
+        UserAccount userAccount = userAccountingRepository.findById(login).orElseThrow();
+        return modelMapper.map(userAccount, UserDto.class);
     }
 }
